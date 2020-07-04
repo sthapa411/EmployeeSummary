c04835f10e6bf4e88476f3251fe47068d12b8003
sthapa <san20hal@gmail.com>
2020-07-03 23:39:42 -0400

    manager js added

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,22 @@
+const Employee = require("./employee");
+class Manager extends Employee {
+
+    constructor(name, id, email, officeNumber) {
+         //super gets the employee properties
+        super(name, id, email);
+         //this. adds the employee properties
+        this.officeNumber = officeNumber;
+
+    };
+
+       //returns office number
+    getOfficeNumber() {
+        return this.officeNumber;
+    };
+    //returns role
+    getRole() {
+        return "Manager";
+    };
+
+};
+module.exports = Manager;
