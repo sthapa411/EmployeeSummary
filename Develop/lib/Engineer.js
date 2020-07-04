a78a6246eebe7cfcfddeb60e8da451fe1cb2eff2
sthapa <san20hal@gmail.com>
2020-07-03 23:35:33 -0400

    inter js added

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,19 +2,20 @@ const Employee = require("./employee");
 class Engineer extends Employee {
 
     constructor(name, id, email, github) {
-
+         //super gets the employee properties
         super(name, id, email);
-    
+        //this. adds the employee properties
         this.github = github;
        
 
     };
 
-
+      //returns github 
     getGithub() {
         return this.github;
     };
 
+      //returns the role
     getRole() {
         return "Engineer";
     };
