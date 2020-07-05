d645e23e10a1f38f9f4763d09014e806edbb7eb9
sthapa <san20hal@gmail.com>
2020-07-05 16:09:41 -0400

    few change made in appJS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const Employee = require("./lib/Employee.js")
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 var teamList = [];
 const managerQuestions = [
